9dfacd8cdb1a0bc5d9a69020fd46ddd10dcbd65b
Muniz Neto <muniznetto@outlook.com>
2022-04-05 22:40:50 -0300

    adicionando menu de opções

---
+++ jogos/forca.py
@@ -0,0 +1,10 @@
+
+def jogar():
+    print('###############################')
+    print('BEM VINDO AO JOGO DE ADVINHAÇÃO')
+    print('###############################')
+    print('fim do jogo')
+
+
+if(__name__ == '__main__'):
+    jogar()    
