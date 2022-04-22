264225b284878c7bba759384ab7f8c110adc5dc5
Muniz Neto <muniznetto@outlook.com>
2022-04-21 21:06:24 -0300

    Melhorando a entrada do usuário

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,9 +1,28 @@
 
+from operator import index
+
+
 def jogar():
     print('###############################')
     print('BEM VINDO AO JOGO DE ADVINHAÇÃO')
     print('###############################')
     print('fim do jogo')
+
+    palavra_secreta = 'banana'
+    enforcou = False
+    acertou = False
+
+    
+    while(not enforcou and not acertou):
+        chute = input('Qual letra?')
+        chute = chute.strip()
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper):
+                print('Encontrei a letra {} na posição {}.'.format(letra, index))  
+            index = index + 1    
+
+    print('Fim do jogo')
 
 
 if(__name__ == '__main__'):
