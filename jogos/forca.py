61c753d1e58cf2431ccdcce300f4be9c057fb2bb
Muniz Neto <muniznetto@outlook.com>
2022-04-21 23:20:18 -0300

    Criando a regra do jogo

--- jogos/forca.py
+++ jogos/forca.py
@@ -8,23 +8,36 @@ def jogar():
     print('###############################')
     print('fim do jogo')
 
-    palavra_secreta = 'banana'
-    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    palavra_secreta = 'palavrer'.upper()
+    letras_acertadas = ['_' for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
     
     while(not enforcou and not acertou):
         chute = input('Qual letra? ')
-        chute = chute.strip()
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra   
-            index = index + 1    
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra   
+                index += 1
+        else:
+            erros += 1  
+
+        enforcou = erros == 6
+        acertou = '_' not in letras_acertadas
         print(letras_acertadas)
+
+    if(acertou):
+        print('vc ganhou')
+    else:
+        print('vc perdeu!!')    
     
     print('fim do jogo')
 
